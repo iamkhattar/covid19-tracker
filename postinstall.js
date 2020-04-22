a1a552cd2aa8229f6a54bf006b78bb36d27444ae
iamkhattar <shivamkhattar1@gmail.com>
2020-04-22 20:12:49 +0100

    Post Install Fixed

--- postinstall.js
+++ postinstall.js
@@ -1,30 +1,42 @@
 const chalk = require("chalk");
-console.log(
-  "+----------------------------------------------------------------+"
-);
-console.log(
-  "|                       " +
-    chalk.bold.green("coronavirus-global") +
-    "                       |"
-);
-console.log(
-  "+----------------------------------------------------------------+"
-);
-console.log(
-  chalk.italic(
-    "|    A tool to view COVID-19 statistics from the command line    |"
-  )
-);
-console.log(
-  "+----------------------------------------------------------------+"
-);
-console.log(
-  "|            Created by " +
-    chalk.blue.bold("Shivam Khattar") +
-    " & " +
-    chalk.blue.bold("Saeed Khan") +
-    "              |"
-);
-console.log(
-  "+----------------------------------------------------------------+"
-);
+const Image = require("ascii-art-image");
+
+var image = new Image({
+  filepath: "./assets/logo.png",
+  alphabet: "greyscale",
+  width: 65,
+  height: 65,
+});
+
+image.write(function (err, rendered) {
+  console.log(rendered);
+  console.log(
+    "+----------------------------------------------------------------+"
+  );
+  console.log(
+    "|                       " +
+      chalk.bold.green("coronavirus-global") +
+      "                       |"
+  );
+  console.log(
+    "+----------------------------------------------------------------+"
+  );
+  console.log(
+    chalk.italic(
+      "|    A tool to view COVID-19 statistics from the command line    |"
+    )
+  );
+  console.log(
+    "+----------------------------------------------------------------+"
+  );
+  console.log(
+    "|            Created by " +
+      chalk.cyan.bold("Shivam Khattar") +
+      " & " +
+      chalk.cyan.bold("Saeed Khan") +
+      "              |"
+  );
+  console.log(
+    "+----------------------------------------------------------------+"
+  );
+});
