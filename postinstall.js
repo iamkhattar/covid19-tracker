55077f19e52c37a29d228535188e741fc703e02f
iamkhattar <shivamkhattar1@gmail.com>
2020-04-22 03:46:25 +0100

    Readme and post install script added

---
+++ postinstall.js
@@ -0,0 +1,30 @@
+const chalk = require("chalk");
+console.log(
+  "+----------------------------------------------------------------+"
+);
+console.log(
+  "|                         " +
+    chalk.bold.green("covid19-tracker") +
+    "                        |"
+);
+console.log(
+  "+----------------------------------------------------------------+"
+);
+console.log(
+  chalk.italic(
+    "|    A tool to view COVID-19 statistics from the command line    |"
+  )
+);
+console.log(
+  "+----------------------------------------------------------------+"
+);
+console.log(
+  "|            Created by " +
+    chalk.blue.bold("Shivam Khattar") +
+    " & " +
+    chalk.blue.bold("Saeed Khan") +
+    "              |"
+);
+console.log(
+  "+----------------------------------------------------------------+"
+);
